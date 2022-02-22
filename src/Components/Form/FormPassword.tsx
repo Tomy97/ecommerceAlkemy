b01f4581c6a7684f6a3f0613bc9fbec9730d3a97
Tomas <tomas_padulo@div-it.com.ar>
2022-02-22 02:21:11 -0300

    mas cambios

---
+++ src/Components/Form/FormPassword.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormPassword = () => {
+  return (
+    <div>FormPassword</div>
+  )
+}
+
+export default FormPassword
---
+++ src/Components/Form/FormPersonalDates.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormPersonalDates = () => {
+  return (
+    <div>FormPersonalDates</div>
+  )
+}
+
+export default FormPersonalDates
